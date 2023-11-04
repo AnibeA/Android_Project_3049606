7f5bb1aa3584c669e3cbfbe40741adf4b5b19a82
AnibeA <anibeojonugwa.alhassan@student.griffith.ie>
2023-11-04 00:00:06 +0000

    First Commit

---
+++ app/src/main/java/com/griffith/a3049606/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.griffith.a3049606
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
